e17325ca3afaa8f382805d3d5058901805c87dea
heaphopper <tzvienir@gmail.com>
2024-12-30 13:04:41 +0200

    add `days` to the forecast Models

--- weather_forecast/forecast.py
+++ weather_forecast/forecast.py
@@ -17,19 +17,22 @@ class HourlyForecast(BaseModel):
     latitude:   float
     longitude:  float
 
-    time: list(str) | None
-    temperature: list(float) | None
-    humidity: list(int) | None
-    rain_prob: list(float) | None
-    wind_speed: list(float) | None
-    wind_direction: list(int) | None
+    days: int
+    time: list[str] | None
+
+    temperature: list[float] | None
+    humidity: list[int] | None
+    rain_prob: list[float] | None
+    wind_speed: list[float] | None
+    wind_direction: list[int] | None
 
 class DailyForecast(BaseModel):
     latitude:   float
     longitude:  float
 
-    time: list(str) | None
-    weather_codes: list(int) | None
-    sunrise: list(str) | None
-    sunset: list(str) | None
-    uv_index: list(float) | None
+    days: int = 7
+    time: list[str] | None
+    weather_codes: list[int] | None
+    sunrise: list[str] | None
+    sunset: list[str] | None
+    uv_index: list[float] | None
