699c7e74e6bad88d6f23f5ffb67f1a4e4585d27d
heaphopper <tzvienir@gmail.com>
2024-12-31 08:59:38 +0200

    adding a hourly data forecast widget

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from city_geolocation.city_widget import *
+from weather_forecast.forecast_widget import *
 
 if __name__ == "__main__":
 
     st.title("Hakol BesWeather!")
-    city_choose_method()
+    city = city_choose_method()
+
+    if(city is not None):
+        hourly_data_widget(city.latitude,city.longitude)
+
