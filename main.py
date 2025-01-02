3bfc107d346feb82968585fe02d0fc19bd917a01
heaphopper <tzvienir@gmail.com>
2025-01-03 01:53:04 +0200

    main

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit
+import geocoder
+
 
 from city_geolocation.city_widget import *
 from weather_forecast.forecast_widget import *
@@ -6,10 +8,17 @@ from weather_forecast.forecast_widget import *
 if __name__ == "__main__":
 
     st.title("Hakol BesWeather!")
+
+    g = geocoder.ip("me")
+    device_lat, device_lon = g.geojson["features"][0]["geometry"]["coordinates"]
+    device_city = g.geojson["features"][0]["properties"]["city"]
+    print(device_lat,device_lon,device_city)
+
     city = city_choose_method()
 
     if(city is not None):
         current_weather_widget(city.country,city.city,city.latitude,city.longitude)
         hourly_data_widget(city.latitude,city.longitude)
         hourly_wind_data_widget(city.latitude,city.longitude)
+        daily_data_widget(city.latitude,city.longitude)
 
