ebb63a14b9e695bc19de2147a9de3978a9651979
heaphopper <tzvienir@gmail.com>
2025-01-03 01:52:26 +0200

    user city geolocation

--- city_geolocation/city_widget.py
+++ city_geolocation/city_widget.py
@@ -1,18 +1,19 @@
 import streamlit as st
+import geocoder
 
 from city_geolocation.city_controller import *
 
 def city_choose_method():
     st.subheader("Pick your location!")
     # Radio button options
-    options = ["By Name", "By Location (lat/lon)"]
+    options = ["Search city", "Get weather info by location coordinates (lat/lon)"]
     # Adding the radio button
     choice = st.radio("Pick a method to set the location for your weather update:", options)
     # Displaying the result based on user choice
     city = None
-    if choice == "By Name":
+    if choice == "Search city":
         city = city_by_name_widget()
-    elif choice == "By Location (lat/lon)":
+    elif choice == "Get weather info by location coordinates (lat/lon)":
         city = city_by_location_widget()
 
     return city
@@ -21,15 +22,15 @@ def city_choose_method():
 def city_by_name_widget():
     # Streamlit widget to input a city name
     st.write("Choosing location by name.")
-    user_city_name = st.text_input("Enter a city name:")
+    user_city_name = st.text_input("Enter a city name:", value=get_current_city())
     user_city = handle_city_name(user_city_name)
     #city_widget(user_city)
     return user_city
 
 def city_by_location_widget():
     st.write("Choosing location by latitude/longitude.")
-    user_city_lat = st.number_input("Enter a latitude:", format="%.7f", value=31.7788242)
-    user_city_lon = st.number_input("Enter a longitude:", format="%.7f", value=35.2257626)
+    user_city_lat = st.number_input("Enter a latitude:", format="%.7f", value=get_current_location()[0])
+    user_city_lon = st.number_input("Enter a longitude:", format="%.7f", value=get_current_location()[1])
     user_city2 = handle_city_location(user_city_lat, user_city_lon)
     #city_widget(user_city2)
     return user_city2
@@ -45,3 +46,13 @@ def city_widget(city):
         **Longitude**: {longitude}
         """
         st.markdown(output)  # Display the aggregated information
+
+def get_current_city():
+    g = geocoder.ip("me")
+    device_city = g.geojson["features"][0]["properties"]["city"]
+    return device_city
+
+def get_current_location():
+    g = geocoder.ip("me")
+    device_lon, device_lat = g.geojson["features"][0]["geometry"]["coordinates"]
+    return device_lat, device_lon
